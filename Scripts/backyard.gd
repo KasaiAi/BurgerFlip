5920b8ad180f1f0d96dec48bf21b75bf22d12a7e
KasaiAi <andre.link.teodoro@gmail.com>
2024-07-03 23:30:00 -0300

    Updated to 4.1; AT LAST, THE RAYCAST

--- Scripts/backyard.gd
+++ Scripts/backyard.gd
@@ -5,10 +5,8 @@ var mouse_pos = Vector2()
 
 func _input(event):
 	if event.is_action_pressed("click"):
-#		var from = $RayCast.global_transform.origin
-#		var to = from + $RayCast.global_transform.basis.z * 100.0
 		print("click")
-		var collision = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(transform.origin, Vector3(0, 0, -20)))
+		var collision = get_world_3d().get_direct_space_state().intersect_ray(PhysicsRayQueryParameters3D.create(transform.origin, Vector3(0, 0, -20)))
 		
 		if collision:
 			print(collision.position)
@@ -22,3 +20,17 @@ func _process(delta):
 	if is_dragging:
 		var new_pos = get_viewport().get_mouse_position() - mouse_pos
 		set_position(new_pos)
+
+func raycast_object():
+	var spaceState = get_world_3d().get_direct_space_state()
+	var mousePos = get_viewport().get_mouse_position()
+	var camera = $Camera3D
+	
+	var rayOrigin = camera.project_ray_origin(mousePos)
+	var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 200
+	
+	var intersect = PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd)
+	var ray = spaceState.intersect_ray(intersect)
+	
+	if ray:
+		return ray.collider.getparent()
